0c278ec33cf8cc0ea04b545485adfdd4a5c51246
Ali Efe GÜR <ali.efe.gur@hotmail.com>
2025-07-04 00:41:46 +0300

    Little Preperation for Shared Library Build

--- ZenithEngine/src/Zenith/API.h
+++ ZenithEngine/src/Zenith/API.h
@@ -5,3 +5,18 @@
 #else
 	#define ZENITH_DEBUG false
 #endif
+
+#ifdef _WIN32
+	#define ZENITH_PLATFORM_WINDOWS
+#ifdef ZENITH_SHARED
+	#ifdef ZENITH_BUILD_DLL
+		#define ZENITH_API __declspec(dllexport)
+	#else
+		#define ZENITH_API __declspec(dllimport)
+	#endif
+#else
+#define ZENITH_API
+#endif
+#else
+	#error "Zenith Engine currently only supports Windows platform."
+#endif
