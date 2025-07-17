fe8a8f763be3e3a0b525247925ba71117479a07c
aliefegur <ali.efe.gur@hotmail.com>
2025-07-17 21:50:53 +0300

    Compiling TestApp (There is Segmentation Fault)

--- ZenithEngine/src/ZenithEngine.h
+++ ZenithEngine/src/ZenithEngine.h
@@ -25,3 +25,10 @@
 #include "Zenith/Utils/File.h"
 
 #include <imgui.h>
+
+#if ZENITH_PLATFORM_WINDOWS
+#include <Windows.h>
+#define ENTRY_POINT int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+#else
+#define ENTRY_POINT int main(int argc, char** argv)
+#endif
